4ac9b80688d4f4a678135f0dd306d9108b5387b6
Arthur Carson <rthrcrsn@gmail.com>
2020-08-28 18:03:19 -0500

    Added inputs for first name last name id title and salary

--- workingfiles/script.js
+++ workingfiles/script.js
@@ -14,18 +14,31 @@ $(document).on('click', '#submitButton', submitInputs);
 function addInputForm() {
     $('body').append(`<h1>Add Employee</h1>`)
     console.log('this is doing something');
-    //add first name
+    //add inputs
     $('body').append(`<input id="firstNameInput" placeholder="First Name"></input>`);
-    
+    $('body').append(`<input id="lastNameInput" placeholder="Last Name"></input>`);
+    $('body').append(`<input id="idNumber" placeholder="ID"></input>`);
+    $('body').append(`<input id="jobTitle" placeholder="Title"></input>`);
+    $('body').append(`<input id="annualSalary" placeholder="Salary"></input>`);
+    //add submit
     $('body').append(`<button id="submitButton">Submit</button>`);
-
 }
 
 function submitInputs() {
     console.log($('#firstNameInput').val());
+    console.log($('#lastNameInput').val());
+    console.log($('#idNumber').val());
+    console.log($('#jobTitle').val());
+    console.log($('#annualSalary').val());
+
    let newEmployeeInfo = {
-       name: $('#firstNameInput').val()
+       firstName: $('#firstNameInput').val(),
+       lastName: $('#lastNameInput').val(),
+       id: $('#idNumber').val(),
+       title: $('#jobTitle').val(),
+       salary: $('#annualSalary').val()
    }
+
    employees.push(newEmployeeInfo);
 }
 
