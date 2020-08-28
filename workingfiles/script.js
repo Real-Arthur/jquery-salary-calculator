d027824b3210b6d48129c2826d957d509077b21f
Arthur Carson <rthrcrsn@gmail.com>
2020-08-28 17:08:20 -0500

    Added form inputs for each category

--- workingfiles/script.js
+++ workingfiles/script.js
@@ -4,4 +4,29 @@ $(document).ready(readyNow);
 
 function readyNow() {
 console.log(`It's a me. jQuery.`)
+addInputForm();
+}
+
+// Add form to html
+function addInputForm() {
+    //Add form
+    $('body').append(`<form></form>`);
+    console.log('this is doing something');
+    //add first name to form
+    $('form').append(`<label for="firstName">First Name</label>
+    <input id="firstNameInput" name="firstName"></input>`);
+    
+    $('form').append(`<label for="lastName">Last Name</label>
+    <input id="lastNameInput" name="lastName"></input>`);
+    
+    $('form').append(`<label for="idNumber">ID</label>
+    <input id="idNumberInput" name="idNumber"></input>`);
+    
+    $('form').append(`<label for="jobTitle">Title</label>
+    <input id="jobTitleInput" name="jobTitle"></input>`);
+    
+    $('form').append(`<label for="annualSalary">Annual Salary</label>
+    <input id="AnnualSalaryInput" name="annualSalary"></input>`);
+
+    $('form').append(`<input type="submit" value="Submit"`)
 }
